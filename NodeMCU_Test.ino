2ddc6dbbe347980c8d02a97322a86bfb136a2cff
Unknown <dave.a.kelly@gmail.com>
2017-11-04 15:02:06 +0000

    Basic Access Point

--- NodeMCU_Test.ino
+++ NodeMCU_Test.ino
@@ -9,25 +9,52 @@
 #include <WiFiClientSecure.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
-
+#include <ESP8266WebServer.h>
 /* Test NodeMCU as a web server for configuring
  *  DK 04/11/17
  *  
  */
+
+const char *ssid = "arduino-er";
+const char *password = "password";
+
+ESP8266WebServer server(80);
+
+char* htmlBody_help = "<h1>Help</h1><br/>\n"
+  "Visit http://192.168.4.1/ to access web server.<br/>\n"
+  "Visit http://192.168.4.1/help to access this page.<br/>\n";
+
+void handleRoot() {
+    server.send(200, "text/html", "<h1>Hello! from arduino-er!</h1>");
+}
+
+void handleHelp(){
+  server.send(200, "text/html", htmlBody_help);
+}
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin (115200);
 
+    WiFi.softAP(ssid, password);
+
+    IPAddress apip = WiFi.softAPIP();
+    Serial.print("visit: \n");
+    Serial.println(apip);
+    server.on("/", handleRoot);
+    server.on("/help", handleHelp);
+    server.begin();
+    Serial.println("HTTP server beginned");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // scan for existing networks:
-  Serial.println("Scanning available networks...");
-  listNetworks();
-  delay(10000);
+  //Serial.println("Scanning available networks...");
+  //listNetworks();
+  //delay(10000);
+    server.handleClient();
 }
 
 void listNetworks() {
