924efc678c456a714901e8d08a1e9937c5ca2d6d
Unknown <dave.a.kelly@gmail.com>
2017-11-04 10:10:37 +0000

    Addded Arduino script

---
+++ NodeMCU_Test.ino
@@ -0,0 +1,15 @@
+/* Test NodeMCU as a web server for configuring
+ *  DK 04/11/17
+ *  
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
